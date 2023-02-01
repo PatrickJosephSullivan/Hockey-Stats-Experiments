6fce21daaa6e9ec6bc2ec4e365e65d462fe0e342
PatrickJosephSullivan <101905657+PatrickJosephSullivan@users.noreply.github.com>
2023-01-31 22:58:16 -0800

    Added server authentication, error present. Will troubleshoot tomorrow.

--- SQL Experiments.py
+++ SQL Experiments.py
@@ -1,2 +1,20 @@
 import mysql.connector
+from mysql.connector import Error
+import pandas as pd
 
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+
+create_server_connection("localhost","root","Poolman40")
